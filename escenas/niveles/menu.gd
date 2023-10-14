0420cdb6737faca2a3bab24aab4e14110a543eff
Matias Aranda <matias_aranda20@hotmail.com>
2023-10-14 12:42:29 -0300

    creacion de menu principal
    se creo un menu con 3 botones: play, creditos y quit, todos con funcionalidad.

---
+++ escenas/niveles/menu.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://escenas/niveles/game.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_creditos_pressed():
+	get_tree().change_scene_to_file("res://escenas/niveles/creditos.tscn")
