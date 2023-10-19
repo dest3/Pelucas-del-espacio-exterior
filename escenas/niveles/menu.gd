d4ea6da9ec28191107b28b7bfa8889ac8555e361
Matias Aranda <matias_aranda20@hotmail.com>
2023-10-19 14:23:01 -0300

    organizado de niveles y objetos
    se eliminaron todos los procesos de objeto y se planteo el esquema de niveles

--- escenas/niveles/menu.gd
+++ escenas/niveles/menu.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://escenas/niveles/Doc_game.tscn")
+	get_tree().change_scene_to_file("res://escenas/niveles/video_intro.tscn")
 
 
 
@@ -16,5 +16,6 @@ func _on_creditos_pressed():
 	get_tree().change_scene_to_file("res://escenas/niveles/creditos.tscn")
 
 
-func _on_play_2_pressed():
+
+func _on_button_pressed():
 	get_tree().change_scene_to_file("res://UI/hud.tscn")
