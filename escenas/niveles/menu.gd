8c259281b8c32cd492a14b9cb30d685548f52df3
Matias Aranda <matias_aranda20@hotmail.com>
2023-10-19 17:16:29 -0300

    agregado de escenas que faltan
    corrección de intercambio de escenas y niveles

--- escenas/niveles/menu.gd
+++ escenas/niveles/menu.gd
@@ -13,23 +13,39 @@ func _on_creditos_pressed():
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://UI/hud.tscn")
 
+#main
+#video intro
 func _on_archivos_pressed():
 	get_tree().change_scene_to_file("res://escenas/niveles/videos/documento_video_intro.tscn")
-
+#mini juego
 func _on_documentos_pressed():
 	get_tree().change_scene_to_file("res://escenas/niveles/Doc_game.tscn")
-
-func _on_maquna_de_escribir_pressed():
-	pass # Replace with function body.
-
-func _on_carpeta_azul_pressed():
-	pass # Replace with function body.
-
-func _on_maquina_de_cine_pressed():
-	pass # Replace with function body.
-
-func _on_menu_pressed():
-	get_tree().change_scene_to_file("res://escenas/niveles/menu.tscn")
-
+#vuelve al menu desde docs
 func _on_tallerdoc_pressed():
 	get_tree().change_scene_to_file("res://UI/hud.tscn")
+
+#video intro
+func _on_maquna_de_escribir_pressed():
+	get_tree().change_scene_to_file("res://escenas/niveles/videos/maquina_de_escribir_intro.tscn")
+#video intro
+func _on_carpeta_azul_pressed():
+	get_tree().change_scene_to_file("res://escenas/niveles/videos/carpeta_azul_video.tscn")
+
+func _on_maquina_de_cine_pressed():
+	get_tree().change_scene_to_file("res://escenas/niveles/videos/maquina_de_video.tscn")
+#vuelve a menu
+func _on_menu_pressed():
+	get_tree().change_scene_to_file("res://escenas/niveles/menu.tscn")
+#vuelve al home desde el juego de documentos
+
+#falta minigame
+func _on_play_maquina_de_escribi_pressed():
+	pass # Replace with function body.
+
+#falta minigame
+func _on_play_carpeta_azul_pressed():
+	pass # Replace with function body.
+
+#falta mini game
+func _on_play_maquina_escribir_pressed():
+	pass # Replace with function body.
