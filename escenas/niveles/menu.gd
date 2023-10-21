c54b8e3ebf9967772fda79840df548f2b3753aac
Matias Aranda <matias_aranda20@hotmail.com>
2023-10-21 20:31:45 -0300

    se agrego efecto visiaual a los botones

--- escenas/niveles/menu.gd
+++ escenas/niveles/menu.gd
@@ -11,7 +11,7 @@ func _on_creditos_pressed():
 	get_tree().change_scene_to_file("res://escenas/niveles/creditos.tscn")
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://UI/hud.tscn")
+	get_tree().change_scene_to_file("res://escenas/niveles/taller.tscn")
 
 func _on_archivos_pressed():
 	get_tree().change_scene_to_file("res://escenas/niveles/videos/documento_video_intro.tscn")
@@ -32,12 +32,49 @@ func _on_menu_pressed():
 	get_tree().change_scene_to_file("res://escenas/niveles/menu.tscn")
 
 func _on_tallerdoc_pressed():
-	get_tree().change_scene_to_file("res://UI/hud.tscn")
+	get_tree().change_scene_to_file("res://escenas/niveles/taller.tscn")
+
+
 
 
 func _on_play_mouse_entered():
-	pass
-
+	var boton = get_node("play/Jugar")
+	boton.scale = Vector2(1.1,1.1)
 
 func _on_play_mouse_exited():
-	pass
+	var boton = get_node("play/Jugar")
+	boton.scale = Vector2(1,1)
+
+func _on_quit_mouse_entered():
+	var boton = get_node("quit/Salir")
+	boton.scale = Vector2(1.1,1.1)
+
+func _on_quit_mouse_exited():
+	var boton = get_node("quit/Salir")
+	boton.scale = Vector2(1,1)
+
+func _on_creditos_mouse_entered():
+	var boton = get_node("creditos/Creditos")
+	boton.scale = Vector2(1.1,1.1)
+
+func _on_creditos_mouse_exited():
+	var boton = get_node("creditos/Creditos")
+	boton.scale = Vector2(1,1)
+
+func _on_menu_mouse_entered():
+	var boton = get_node("menu/Salir")
+	boton.scale = Vector2(1.1,1.1)
+
+func _on_menu_mouse_exited():
+	var boton = get_node("menu/Salir")
+	boton.scale = Vector2(1,1)
+
+
+func _on_crunch_mouse_entered():
+	var boton = get_node("crunch/Crunch")
+	boton.scale = Vector2(1.1,1.1)
+
+
+func _on_crunch_mouse_exited():
+	var boton = get_node("crunch/Crunch")
+	boton.scale = Vector2(1,1)
