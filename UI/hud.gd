bd2d668b28861bd76cbf524a290eec6ee701d59c
Matias Aranda <matias_aranda20@hotmail.com>
2023-10-21 22:18:07 -0300

    implementacion de hud

---
+++ UI/hud.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+
+
+
+func _on_carpeta_azul_pressed():
+	get_tree().change_scene_to_file("res://escenas/niveles/Doc_game.tscn")
+
+
+func _on_maquina_de_escribir_pressed():
+	pass # Replace with function body.
+
+
+func _on_documentos_pressed():
+	pass # Replace with function body.
+
+
+func _on_maquina_de_video_pressed():
+	pass # Replace with function body.
+
+
+func _on_volver_pressed():
+	get_tree().change_scene_to_file("res://escenas/niveles/taller.tscn")
--- escenas/niveles/menu.gd
+++ escenas/niveles/menu.gd
@@ -13,20 +13,6 @@ func _on_creditos_pressed():
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://escenas/niveles/taller.tscn")
 
-func _on_archivos_pressed():
-	get_tree().change_scene_to_file("res://escenas/niveles/videos/documento_video_intro.tscn")
-
-func _on_documentos_pressed():
-	get_tree().change_scene_to_file("res://escenas/niveles/Doc_game.tscn")
-
-func _on_maquna_de_escribir_pressed():
-	pass # Replace with function body.
-
-func _on_carpeta_azul_pressed():
-	pass # Replace with function body.
-
-func _on_maquina_de_cine_pressed():
-	pass # Replace with function body.
 
 func _on_menu_pressed():
 	get_tree().change_scene_to_file("res://escenas/niveles/menu.tscn")
